9694ed775c0ab453b441f09976a524f1d2c84089
Leachenski <jcleachenski@gmail.com>
2024-06-18 08:50:43 -0300

    refactor: adiciona lógica de ordenação e remoção na iteração do primeiro for

--- index.js
+++ index.js
@@ -6,16 +6,7 @@ for (let i = 0; i < numeros.length; i++) {
             let aux = numeros[i]
             numeros[i] = numeros[j]
             numeros[j] = aux
-        }
-
-    }
-}
-
-console.log(numeros)
-
-for (let i = 0; i < numeros.length; i++) {
-    for (let j = i + 1; j < numeros.length; j++) {
-        if(numeros[i] == numeros[j]) {
+        } else if(numeros[i] == numeros[j]) {
             numeros.splice(j, 1)
             j--
         }
